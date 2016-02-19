30ac151587e1591ad9f0f0a7b3f04abe09140c91
Gustav Jorlöv <gustav.jorlov@kits.se>
2016-02-19 19:43:41 +0100

    modified for heroku compliance

--- server/app.js
+++ server/app.js
@@ -1,1 +1,11 @@
-console.log("Yeah!");
+var express = require('express');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+app.get("/", function(req, res){
+	res.json({'o': 'k'});
+});
+
+app.listen(app.get('port'));
