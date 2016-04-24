2253cf472e163c2cf1cdbebcafbe78cc65eeb339
Gustav Jorlöv <gustav.jorlov@kits.se>
2016-04-24 23:27:24 +0200

    more cool stuff

--- server/app.js
+++ server/app.js
@@ -7,16 +7,10 @@ var app = express();
 
 var CLIENT_ID = "1bac9d4c01ff440395c47f39567a8897";
 var CLIENT_SECRET = "d6565fb097a54473aa84092be097aadc";
+var REDIRECT_URI = "https://jossanrickard.herokuapp.com/code";
 var ACCESS_TOKEN = "";
-var oauth = {
-	client_id: CLIENT_ID,
-	client_secret: CLIENT_SECRET,
-	grant_type: 'authorization_code',
-	redirect_uri: '',
-	code: ''
-};
 
-var auth_url = "https://api.instagram.com/oauth/authorize/?client_id="+CLIENT_ID+"&redirect_uri=https://jossanrickard.herokuapp.com/code&response_type=code";
+var auth_url = "https://api.instagram.com/oauth/authorize/?client_id="+CLIENT_ID+"&redirect_uri="+REDIRECT_URI+"&response_type=code";
 var auth_token_url = "https://runkeeper.com/apps/token";
 
 app.set('port', (process.env.PORT || 3000));
@@ -25,20 +19,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/login", function(req, res){
 	if(ACCESS_TOKEN !== ""){
-		res.json({'o': 'k'});
+		res.json({'o': ACCESS_TOKEN});
 	}else{
 		res.redirect(auth_url);
 	}
 });
 
 app.get("/code", function(req, res){
-	console.log("code", req.query.code, JSON.stringify(req.url));
-	console.log(querystring.parse(req.url.split("?")[1]));
-	var code = querystring.parse(req.url.split("?")[1]).code;
-	res.send("Yeah " + code);
-
-	request.post("https://api.instagram.com/oauth/access_token", {}, function(){
-
+	console.log("code", req.query.code);
+	request.post("https://api.instagram.com/oauth/access_token", {
+		form:{
+			client_id: CLIENT_ID,
+			client_secret: CLIENT_SECRET,
+			grant_type: "authorization_code",
+			redirect_uri: REDIRECT_URI,
+			code: req.query.code
+		}
+	}, function(err, response, body){
+		if(err){
+			res.json(err);
+		}else{
+			ACCESS_TOKEN = JSON.parse(body).access_token;
+			res.json(body);
+		}
 	});
 
 });
