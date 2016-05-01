66bc9a0bfcaafb5e0962640a763f641ebdce601c
Gustav Jorlöv <gustav.jorlov@gmail.com>
2016-05-01 18:13:02 +0200

    content

--- webapp/ImageList.js
+++ webapp/ImageList.js
@@ -3,8 +3,11 @@ import Image from './Image.js';
 
 export default class ImageList extends React.Component{
     getImages(item){
-        console.log(item);
-        return "<h1>"+item.url+"</h1>";
+        return (<div key={item.id} className="image">
+            <img src={item.url} />
+            <p>{item.caption}</p>
+            <p>{new Date(item.time)}</p>
+        </div>);
     }
     render(){
         console.log("ImageList", this.state);
