b7675231e993af557c3bdbbb3f65f6f824a53085
Gustav Jorlöv <gustav.jorlov@gmail.com>
2016-05-01 20:56:53 +0200

    typo

--- webapp/Image.js
+++ webapp/Image.js
@@ -2,21 +2,11 @@ import React from 'react';
 
 const Image = ({id, url, caption}) => {
     return (
-        <div key={this.props.id} className="image">
-            <img src={this.props.url} />
-            <p>{this.props.caption}</p>
+        <div key={id} className="image">
+            <img src={url} />
+            <p>{caption}</p>
         </div>
     );
 }
 
 export default Image;
-
-// export default class Image extends React.Component{
-//     render(){
-//         console.log("Image", this.props);
-//         return (<div key={this.props.id} className="image">
-//             <img src={this.props.url} />
-//             <p>{this.props.caption}</p>
-//         </div>);
-//     }
-// }
