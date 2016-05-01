67300d65d63ada5574c70e4e2f06682e32e9df13
Gustav Jorlöv <gustav.jorlov@gmail.com>
2016-05-01 20:54:37 +0200

    changed into stateless function for the Image component

--- webapp/Image.js
+++ webapp/Image.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-export default class Image extends React.Component{
-    render(){
-        console.log("Image", this.props);
-        return (<div key={this.props.id} className="image">
+const Image = ({id, url, caption}) => {
+    return (
+        <div key={this.props.id} className="image">
             <img src={this.props.url} />
             <p>{this.props.caption}</p>
-        </div>);
-    }
+        </div>
+    );
 }
+
+export default Image;
+
+// export default class Image extends React.Component{
+//     render(){
+//         console.log("Image", this.props);
+//         return (<div key={this.props.id} className="image">
+//             <img src={this.props.url} />
+//             <p>{this.props.caption}</p>
+//         </div>);
+//     }
+// }
