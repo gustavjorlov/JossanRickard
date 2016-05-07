ad40d3bff63bb429ad62d4cced4c82a74096abed
Gustav Jorlöv <gustav.jorlov@kits.se>
2016-05-07 21:16:17 +0200

    more text stuff

--- webapp/Image.js
+++ webapp/Image.js
@@ -4,7 +4,7 @@ const Image = ({id, url, caption}) => {
     return (
         <figure key={id} className="figure">
             <img src={url} className="figure-img img-fluid img-rounded" />
-            <figcaption className="figure-caption text-center-lg">{caption}</figcaption>
+            <h2>{caption}</h2>
         </figure>
     );
 }
