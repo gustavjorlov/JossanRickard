e4a33b1191238dbb2d156786f5c6f594674f7304
Gustav Jorlöv <gustav.jorlov@kits.se>
2016-05-09 21:07:52 +0200

    simplified layout

--- webapp/Image.js
+++ webapp/Image.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Image = ({id, url, caption, displayname}) => {
     return (
         <figure key={id} className="figure col-sm-6">
-            <h2>{displayname}</h2>
             <img src={url} className="figure-img img-fluid img-rounded" />
         </figure>
     );
