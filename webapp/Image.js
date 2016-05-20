e7c31fa61a38b360a4a5ebcf16d913e1816293f4
Gustav Jorlöv <gustav.jorlov@kits.se>
2016-05-20 15:04:11 +0200

    react typo

--- webapp/Image.js
+++ webapp/Image.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Image = ({id, url, caption, displayname}) => {
     return (
-        <figure key={id} className="figure {window.location.hash === '#two' ? 'col-sm-6' : ''}">
+        <figure key={id} className={'figure ' (window.location.hash === '#two' ? 'col-sm-6' : '')}>
             <img src={url} className="figure-img img-fluid img-rounded" />
         </figure>
     );
