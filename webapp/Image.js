0181eefe8de8cc87fa473dc3a85c3d3622592c54
Gustav Jorlöv <gustav.jorlov@kits.se>
2016-05-20 14:53:02 +0200

    changed back to 1 column

--- webapp/Image.js
+++ webapp/Image.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Image = ({id, url, caption, displayname}) => {
     return (
-        <figure key={id} className="figure col-sm-6">
+        <figure key={id} className="figure">
             <img src={url} className="figure-img img-fluid img-rounded" />
         </figure>
     );
